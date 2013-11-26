f9a6b47c89375810df18b911e30b42210a20e58b
Nicolas <nthomas@robotikdose.com>
2013-11-26 17:24:14 +0100

    26111724

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,20 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope) {
   // Main app controller, empty for the example
+  
+    $scope.$on('tab.shown', function() {
+    // Might do a load here
+    alert('tot');
+     var myLatlng = new google.maps.LatLng( 51.520838, -0.140261 );
+            var myOptions = {
+                zoom: 15,
+                center: myLatlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            var map = new google.maps.Map( document.getElementById( "map_canvas" ), myOptions );
+  });
+  
+  
 })
 
 // A simple controller that fetches a list of data
